bfb0503e0341c0148f17075648388aa4787af422
DavidJCobb <831497+DavidJCobb@users.noreply.github.com>
2018-11-04 13:34:25 +0100

    Add files via upload
    better documentation, especially regarding shield bleedthrough

--- SKSE Plugin/CobbAPI/Patches/Exploratory.h
+++ SKSE Plugin/CobbAPI/Patches/Exploratory.h
@@ -13,6 +13,9 @@ namespace CobbPatches {
       //    but still have low health, then you can still be killmoved. We'd need 
       //    to fix that in order to implement "energy shields" reliably.
       //
+      //  - There is no "shield regen delay." Magicka constantly regenerates even 
+      //    as you take damage.
+      //
       void _stdcall Inner(RE::Actor* target, float* result) {
          float currentHP = target->actorValueOwner.GetCurrent(0x18);
          float currentMG = target->actorValueOwner.GetCurrent(0x19);
@@ -22,7 +25,15 @@ namespace CobbPatches {
             return;
          }
          //
-         // Say we have 50 magicka and are losing 70 health: 50 + -70 = -20
+         // Code below does shield bleedthrough. If we don't want bleedthrough, then 
+         // after setting magicka to zero, we'd need to return zero if magicka is zero, 
+         // and return (result) otherwise. (Actually, magicka always regens. If we don't 
+         // patch in a shield regen delay, then we'd need to return (result) if magicka 
+         // is below some threshold; otherwise, it'll very rarely be at zero, so actors 
+         // will be nigh immortal.)
+         //
+         // So: Say we have 50 magicka and are losing 70 health. 50 + -70 = -20, so we'd 
+         // return -20 for bleedthrough.
          //
          float healthMod = currentMG + *result;
          target->actorValueOwner.ModifyModifier(RE::kAVModifier_Damage, 0x19, -currentMG);

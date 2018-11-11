d09dc05cdd2168f21b7b94f5c0c50343ee0b76f4
DavidJCobb <831497+DavidJCobb@users.noreply.github.com>
2018-11-11 20:59:50 +0100

    Add files via upload
    Fix-up now that the BGSSaveLoadManager struct is named

--- SKSE Plugin/CobbAPI/Utilities/ReferenceExists.h
+++ SKSE Plugin/CobbAPI/Utilities/ReferenceExists.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <exception>
 #include "ReverseEngineered/Systems/ChangeForms.h"
+#include "ReverseEngineered/Systems/Savedata/BGSSaveLoadManager.h"
 
 static bool ReferenceWithIDExists(UInt32 formID){
    if (LookupFormByID(formID) != NULL)
@@ -11,10 +12,10 @@ static bool ReferenceWithIDExists(UInt32 formID){
    // LookupFormByID can't retrieve unloaded temporary references and returns NULL for them. We'll check the changeforms.
    //
    {
-      RE::Unknown01B2E38C* Unk01B2E38C = RE::Unknown01B2E38C::GetInstance();
-      if (Unk01B2E38C == NULL)
+      RE::BGSSaveLoadManager* man = RE::BGSSaveLoadManager::GetSingleton();
+      if (man == nullptr)
          throw new std::runtime_error("Couldn't access changeFlags.");
       UInt32 changeFlags;
-      return CALL_MEMBER_FN(Unk01B2E38C->unk3E8, GetChangeFlags)(&changeFlags, formID); // returns true if it can retrieve the change flags
+      return CALL_MEMBER_FN(man->unk3E8, GetChangeFlags)(&changeFlags, formID); // returns true if it can retrieve the change flags
    }
 };
